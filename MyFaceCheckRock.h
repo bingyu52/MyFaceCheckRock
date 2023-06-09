10a9c03d1c4035d137017baa2c69a020ffb03309
bingyu52 <125442047+bingyu52@users.noreply.github.com>
2023-06-09 14:41:23 +0800

    上传项目：人脸识别系统
    把项目人脸识别系统同步到仓库

---
+++ MyFaceCheckRock.h
@@ -0,0 +1,32 @@
+﻿
+// MyFaceCheckRock.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CMyFaceCheckRockApp:
+// 有关此类的实现，请参阅 MyFaceCheckRock.cpp
+//
+
+class CMyFaceCheckRockApp : public CWinApp
+{
+public:
+	CMyFaceCheckRockApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMyFaceCheckRockApp theApp;
